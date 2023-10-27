eb63f41ebb0bc7daec38f22a5bdaebf9e274539c
Paul Popus <paul@nouance.io>
2023-10-27 01:22:02 +0100

    Fix port

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -9,6 +9,7 @@ import { buildConfig } from 'payload/config'
 import Users from './collections/Users'
 
 export default buildConfig({
+  serverURL: process.env.SERVER_URL,
   admin: {
     user: Users.slug,
     bundler: webpackBundler(),
